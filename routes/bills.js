3246e7f7bd4823fed4a8fa58fd8d974b473d3ae4
Nathan Park <nate.park90@gmail.com>
2015-12-13 15:38:57 -0800

    Create post route for adding committee information, clean up comments from code

--- routes/bills.js
+++ routes/bills.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/*
- * GET bill_list.
- */
 router.get('/bill_list', function(req, res) {
     var db = req.db;
     var collection = db.get('bill_list');
@@ -12,12 +9,19 @@ router.get('/bill_list', function(req, res) {
     });
 });
 
-/*
- * POST to add_bill.
- */
 router.post('/add_bill', function(req, res) {
     var db = req.db;
-    var collection = db.get('bill_list'); // get collection
+    var collection = db.get('bill_list');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+router.post('/add_committee', function(req, res) {
+    var db = req.db;
+    var collection = db.get('bill_list');
     collection.insert(req.body, function(err, result){
         res.send(
             (err === null) ? { msg: '' } : { msg: err }
