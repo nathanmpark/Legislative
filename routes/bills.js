ee6fd9dbb16f51b2c30f8b8b532c14038e20f23e
Nathan Park <nate.park90@gmail.com>
2015-12-13 10:17:44 -0800

    Remove show bill info, add post route for adding bill list

--- routes/bills.js
+++ routes/bills.js
@@ -13,10 +13,16 @@ router.get('/bill_list', function(req, res) {
 });
 
 /*
- * GET api.
+ * POST to adduser.
  */
-router.get('/api', function(req, res) {
-    var request = req.params
-    res.json(params)
+router.post('/add_bill', function(req, res) {
+    var db = req.db;
+    var collection = db.get('bill_list');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
 });
+
 module.exports = router;
