0029e52bcc9c7dc9bf5f208871a096f32a686f14
maryengelbrecht <maryengelbrecht@gmail.com>
2015-12-14 10:52:28 -0800

    old branch-failed attempts

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -125,7 +125,10 @@ function addCommittee(bill_info) {
 
 
 
-
-
-
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .text(function(d) { return d; });
 
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,18 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) 
   // res.render('index', { title: 'Express' });
-  {
-  res.render('index', 
-  { 
-    heading: 'Welcome', 
-    welcome: 'This is how easy it is to embed HTML inside Express Views'
-  });
+
+  // {
+  // res.render('index', 
+  // { 
+  //   heading: 'Welcome', 
+  //   welcome: 'This is how easy it is to embed HTML inside Express Views'
+  // });
+
+	res.render('index', 
+		{ 
+			title: 'html' 
+		});
 });
 
 module.exports = router;
