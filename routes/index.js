478a9cdb50355bcec29e223f8204aad6a93f58cd
maryengelbrecht <maryengelbrecht@gmail.com>
2015-12-14 13:03:26 -0800

    more trash

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) 
-  // res.render('index', { title: 'Express' });
-
-  {
-  res.render('index', 
-  { 
-    heading: 'Welcome', 
-    welcome: 'This is how easy it is to embed HTML inside Express Views'
-  });
-
-	// res.render('index', 
-	// 	{ 
-	// 		title: 'html' 
-	// 	});
+router.get('/', function(req, res, next) {
+  res.render('index');
 });
 
 module.exports = router;
