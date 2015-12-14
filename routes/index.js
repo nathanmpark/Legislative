f4d14708b8aa49e6f4b1d0600813611cc5de7b27
Nathan Park <nate.park90@gmail.com>
2015-12-14 11:26:33 -0800

    Add flare.json file, and proof of concept for d3.js bubble chart

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) 
-  // res.render('index', { title: 'Express' });
-  {
-  res.render('index', 
-  { 
-    heading: 'Welcome', 
-    welcome: 'This is how easy it is to embed HTML inside Express Views'
-  });
+router.get('/', function(req, res, next) {
+  res.render('index');
 });
 
 module.exports = router;
