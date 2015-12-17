48e542079d9a811ff858dd37605785e63ba9413b
ctkochan22 <ctkochan22@gmail.com>
2015-12-17 15:11:02 -0800

    Polishes the scrolling effect. Adds navigation functionality for each section.

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -7,46 +7,10 @@ getBills();
 //DOCUMENT READY METHODS
 $(document).ready(function() {
 
-    $('body').scrollspy({ target: '.navbar' });
-    $('.chart-link').on('click', function(e){
-        console.log("Stopping Click");
-        e.preventDefault();
-
-        var hash = this.hash;
-
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function(){
-            window.location.hash = hash;
-        });
-    });
-    cover_hide();
-    navbar_fade($);
 });
 
-//***** UX FUNCTIONS *****
-function navbar_fade(object){
-    // $(document).ready(function(){
-    //     $('.navbar').hide();
-    // });
 
-    $(function() {
-        $(window).scroll(function(){
-            if ($(this).scrollTop() <= 2) {
-                $('.navbar').fadeIn();
-            } else {
-                $('.navbar').fadeOut();
-            };
-        });
-    });
-}//#navbar_fade END
 
-function cover_hide(){
-    $('.cover').on('click', function(e){
-        e.preventDefault();
-        $('.cover').hide();
-    });
-}
 
 
 //***** API CALL FUNCTIONS *****
